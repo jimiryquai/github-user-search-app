661638bccdf389903a5365d555bea82d39a29ffa
jimiryquai <james@jamesryan.dev>
2023-11-08 16:25:29 +0000

    Added dark/light js toggle

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,16 @@
 const colorSchemeLabel = document.querySelector('.header__toggle-label');
 const colorSchemeToggle = document.querySelector('.header__toggle');
-const colorScheme = document.querySelector('#color-scheme');
 
 const darkModeHandler = (e) => e.matches ? colorSchemeLabel.textContent = 'LIGHT' : colorSchemeLabel.textContent = 'DARK';
 
 matchMedia('(prefers-color-scheme: dark)').addEventListener('change', darkModeHandler);
+
+colorSchemeToggle.addEventListener("click", () => {
+  if (matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.body.classList.toggle("light");
+    colorSchemeLabel.textContent = 'LIGHT'
+  } else {
+    document.body.classList.toggle("dark");
+    colorSchemeLabel.textContent = 'DARK'
+  }
+});
