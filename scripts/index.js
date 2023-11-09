e47906867648b083d720f85c02c4adcbf75dc0a9
jimiryquai <james@jamesryan.dev>
2023-11-09 18:47:48 +0000

    added toggleModeHandler function

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,40 @@
-const colorSchemeLabel = document.querySelector('.header__toggle-label');
+const colorSchemeLabel = document.querySelector('[class*="header__toggle-label"]');
 const colorSchemeToggle = document.querySelector('.header__toggle');
+const allElements = Array.from(document.getElementsByTagName("*"));
 
-const darkModeHandler = (e) => e.matches ? colorSchemeLabel.textContent = 'LIGHT' : colorSchemeLabel.textContent = 'DARK';
+const setColorSchemeLabelOnLoad = () => matchMedia("(prefers-color-scheme: light)").matches ? colorSchemeLabel.textContent = 'DARK' : colorSchemeLabel.textContent = 'LIGHT';
 
-matchMedia('(prefers-color-scheme: dark)').addEventListener('change', darkModeHandler);
-
-colorSchemeToggle.addEventListener("click", () => {
-  if (matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.body.classList.toggle("light");
-    colorSchemeLabel.textContent = 'LIGHT'
+const toggleModeHandler = () => {
+  if(colorSchemeLabel.textContent === 'LIGHT') {
+    colorSchemeLabel.textContent = 'DARK';
+    allElements.forEach(element => {
+      element.classList.remove("dark");
+      element.classList.add("light");
+    });
   } else {
-    document.body.classList.toggle("dark");
-    colorSchemeLabel.textContent = 'DARK'
+    if(colorSchemeLabel.textContent === 'DARK') {
+      colorSchemeLabel.textContent = 'LIGHT';
+      allElements.forEach(element => {
+        element.classList.remove("light");
+        element.classList.add("dark");
+      });
+    }
   }
-});
+};
+
+const prefersDarkModeHandler = (e) => {
+  if (e.matches) {
+    colorSchemeLabel.textContent = 'LIGHT';
+  }
+};
+
+const prefersLightModeHandler = (e) => {
+  if (e.matches) {
+    colorSchemeLabel.textContent = 'DARK';
+  }
+};
+
+setColorSchemeLabelOnLoad();
+matchMedia('(prefers-color-scheme: light)').addEventListener('change', prefersLightModeHandler);
+matchMedia('(prefers-color-scheme: dark)').addEventListener('change', prefersDarkModeHandler);
+colorSchemeToggle.addEventListener('click', toggleModeHandler);
